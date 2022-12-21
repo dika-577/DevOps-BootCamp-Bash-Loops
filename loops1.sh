b81a9ead6f0acf206c8abb2e0f24c631ccdd6105
dimitri <korgalidzed@gmail.com>
2022-12-21 16:44:30 +0400

    add code in loops1.sh

--- loops1.sh
+++ loops1.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 # Put your code here
+reversed_string=$(echo "$1" | rev)
+  changed_string=$(echo "$reversed_string" | tr '[:upper:][:lower:]' '[:lower:][:upper:]' )
+  echo "$changed_string"
